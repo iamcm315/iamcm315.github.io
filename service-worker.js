18d66c61014b67d41b6a478c7351c27c2b4cd3ec
Clarence Chan <EDPcmcc@hq.hk.hld>
2020-01-06 12:44:38 +0800

    wip

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2456111d6f8d4cbf635b0fde7c7638ec.js"
+  "/precache-manifest.3d782c81f97bbd63739227dc9c838f7f.js"
 );
 
 workbox.skipWaiting();
